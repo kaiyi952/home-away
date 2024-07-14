10bc3a2b3baa2e30261a26d8a87ced1435ca8512
Kaiyi952 <irishe952@gmail.com>
2024-07-13 23:40:01 -0400

    [update]create/page

--- app/profile/create/page.tsx
+++ app/profile/create/page.tsx
@@ -1,21 +1,21 @@
+import FormContainer from "@/components/form/FormContainer";
 
 
 
-const createProfileAction = async (formData: FormData) => {
+const createProfileAction = async (prevState:any,formData: FormData) => {
     'use server';
     const firstName = formData.get('firstName') as string;
     console.log(firstName);
-    
+    return {message:'Profile Created' }
 }
 
 function CreateProfilePage() {
     return <section>
         <h1 className='text-2xl font-semibold mb-8 capitalize'>new user</h1>
         <div className='border p-8 rounded-md max-w-lg'>
-            <form action={createProfileAction}>
-                
-               
-        </form> 
+            <FormContainer action={createProfileAction}>
+
+            </FormContainer>
         </div>
   </section>
 }
