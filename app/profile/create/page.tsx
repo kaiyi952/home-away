c467d412d64824d27ce3f99f55a8da2491d9cfad
Kaiyi952 <irishe952@gmail.com>
2024-07-14 22:13:51 -0400

    [update] 7.14

--- app/profile/create/page.tsx
+++ app/profile/create/page.tsx
@@ -1,4 +1,6 @@
+import { SubmitButton } from "@/components/form/Buttons";
 import FormContainer from "@/components/form/FormContainer";
+import FormInput from "@/components/form/Forminput";
 
 
 
@@ -12,9 +14,15 @@ const createProfileAction = async (prevState:any,formData: FormData) => {
 function CreateProfilePage() {
     return <section>
         <h1 className='text-2xl font-semibold mb-8 capitalize'>new user</h1>
-        <div className='border p-8 rounded-md max-w-lg'>
+        <div className='border p-8 rounded-md'>
             <FormContainer action={createProfileAction}>
+                <div className="grid md:grid-cols-2 gap-4 mt-4">
 
+                <FormInput type="text" name="firstName" label="First Name" />
+                <FormInput type="text" name="lastName" label="Last Name" />
+                <FormInput type="text" name="username" label="Username" />
+                </div>
+                <SubmitButton text="Create Profile" className="mt-8"/>
             </FormContainer>
         </div>
   </section>
